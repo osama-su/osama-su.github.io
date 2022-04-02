81afad9a718d4a179e54091d0390a44bb81d9c08
Osama <osamasahmed.sus@gmail.com>
2022-04-02 23:36:09 +0000

    about component

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,8 +1,51 @@
 import React from "react";
 import "./about.css";
+import ME from "../../assets/me.png";
+import { FaProjectDiagram } from "react-icons/fa";
+import { FiUsers } from "react-icons/fi";
+import { BsFolderCheck } from "react-icons/bs";
+import CallToAction from "../header/CallToAction";
 
 const About = () => {
-  return <section id="about">About</section>;
+  return (
+    <section id="about">
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+      <div className="container about-container">
+        <div className="about-me">
+          <div className="about-me-image">
+            <img src={ME} alt="about" />
+          </div>
+        </div>
+        <div className="about-content">
+          <div className="about-cards">
+            <article className="about-card">
+              <FaProjectDiagram className="about-card-icon" />
+              <h5>Experience</h5>
+              <small>3+ Years Working</small>
+            </article>
+            <article className="about-card">
+              <FiUsers className="about-card-icon" />
+              <h5>Clients</h5>
+              <small>200+ Worldwide</small>
+            </article>
+            <article className="about-card">
+              <BsFolderCheck className="about-card-icon" />
+              <h5>Projects</h5>
+              <small>80+ Completed Projects</small>
+            </article>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Repudiandae, similique doloremque! Cupiditate animi corporis
+            possimus similique sequi distinctio, cum saepe dicta. Ullam
+            perspiciatis quam eos molestias voluptates velit, harum magnam?
+          </p>
+          <CallToAction />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
