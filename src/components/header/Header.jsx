8070080e57297c17161db2ed39d8958baec7ad51
Osama <osamasahmed.sus@gmail.com>
2022-04-02 03:34:39 +0000

    header component styling

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import "./header.css";
 import CallToAction from "./CallToAction";
-
+import Socials from "./Socials";
+import Me from "../../assets/me.png";
 const Header = () => {
   return (
     <header>
@@ -10,7 +11,10 @@ const Header = () => {
         <h1>Osama Ahmed </h1>
         <h5 className="text-light">Full Stack Developer</h5>
         <CallToAction />
-
+        <div className="me">
+          <img src={Me} alt="me" />
+        </div>
+        <Socials />
         <a href="#contact" className="scroll-down">
           Scroll Down
         </a>
--- src/components/header/Socials.jsx
+++ src/components/header/Socials.jsx
@@ -1,20 +1,21 @@
 import React from "react";
+import { BsLinkedin, BsGithub, BsTwitter } from "react-icons/bs";
 
 const Socials = () => {
   return (
     <div className="header-socials">
       <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
-        l
+        <BsLinkedin />
       </a>
       <a
         href="https://github.com/osama-su"
         target="_blank"
         rel="noopener noreferrer"
       >
-        g
+        <BsGithub />
       </a>
       <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
-        t
+        <BsTwitter />
       </a>
     </div>
   );
