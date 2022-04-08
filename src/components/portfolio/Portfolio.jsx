31088ae883e1f7098250eb8ecefc1060db407a90
Osama <osamasahmed.sus@gmail.com>
2022-04-08 19:16:11 +0000

    portfolio component

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -2,7 +2,44 @@ import React from "react";
 import "./portfolio.css";
 
 const Portfolio = () => {
-  return <section id="portfolio">Portfolio</section>;
+  return (
+    <section id="portfolio">
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+      <div className="container portfolio-container">
+        <article className="portfolio-item">
+          <div className="portfolio-item-image"></div>
+          <h3>Project Title</h3>
+          <a href="https://github.com/osama-su/2048-clone" className="btn">
+            Github
+          </a>
+          <a
+            href="https://osamaahmed.me/2048-clone"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="btn btn-primary"
+          >
+            Live Demo
+          </a>
+        </article>
+        <article className="portfolio-item">
+          <div className="portfolio-item-image"></div>
+          <h3>Project Title</h3>
+          <a href="https://github.com/osama-su/portfolio-v0.1" className="btn">
+            Github
+          </a>
+          <a
+            href="https://osamaahmed.me/portfolio-v0.1"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="btn btn-primary"
+          >
+            Live Demo
+          </a>
+        </article>
+      </div>
+    </section>
+  );
 };
 
 export default Portfolio;
