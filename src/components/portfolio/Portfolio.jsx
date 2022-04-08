19e9c5f9e7fb887e96e66e0f7f53aa57e0159025
Osama <osamasahmed.sus@gmail.com>
2022-04-08 19:40:11 +0000

    portfolio component style

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./portfolio.css";
+import Two048Clone from "../../assets/2048-clone.png";
+import PortfolioV01 from "../../assets/portfolio-v0.1.png";
 
 const Portfolio = () => {
   return (
@@ -8,34 +10,45 @@ const Portfolio = () => {
       <h2>Portfolio</h2>
       <div className="container portfolio-container">
         <article className="portfolio-item">
-          <div className="portfolio-item-image"></div>
+          <div className="portfolio-item-image">
+            <img src={Two048Clone} alt="2048 Clone" />
+          </div>
           <h3>Project Title</h3>
-          <a href="https://github.com/osama-su/2048-clone" className="btn">
-            Github
-          </a>
-          <a
-            href="https://osamaahmed.me/2048-clone"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="btn btn-primary"
-          >
-            Live Demo
-          </a>
+          <div className="portfolio-item-cta">
+            <a href="https://github.com/osama-su/2048-clone" className="btn">
+              Github
+            </a>
+            <a
+              href="https://osamaahmed.me/2048-clone"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="btn btn-primary"
+            >
+              Live Demo
+            </a>
+          </div>
         </article>
         <article className="portfolio-item">
-          <div className="portfolio-item-image"></div>
+          <div className="portfolio-item-image">
+            <img src={PortfolioV01} alt="Portfolio v0.1" />
+          </div>
           <h3>Project Title</h3>
-          <a href="https://github.com/osama-su/portfolio-v0.1" className="btn">
-            Github
-          </a>
-          <a
-            href="https://osamaahmed.me/portfolio-v0.1"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="btn btn-primary"
-          >
-            Live Demo
-          </a>
+          <div className="portfolio-item-cta">
+            <a
+              href="https://github.com/osama-su/portfolio-v0.1"
+              className="btn"
+            >
+              Github
+            </a>
+            <a
+              href="https://osamaahmed.me/portfolio-v0.1"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="btn btn-primary"
+            >
+              Live Demo
+            </a>
+          </div>
         </article>
       </div>
     </section>
